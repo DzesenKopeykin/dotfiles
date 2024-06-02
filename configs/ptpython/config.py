52c025aedde1e7f1adf30d5dcda84431df21ca24
DzesenKopeykin <kopeykindzzesen@gmail.com>
2024-06-02 20:52:27 +0300

    change config for ptpython

--- configs/ptpython/config.py
+++ configs/ptpython/config.py
@@ -1,3 +1,12 @@
+from ptpython.layout import CompletionVisualisation
+
+
 def configure(repl):
+    repl.completion_visualisation = CompletionVisualisation.TOOLBAR
+    repl.show_line_numbers = True
+    repl.enable_fuzzy_completion = True
+    repl.enable_dictionary_completion = True
+    repl.enable_auto_suggest = True
     repl.vi_mode = True
-    repl.use_code_colorscheme("friendly")
+    repl.use_code_colorscheme("native")
+    repl.max_brightness = 0.7
